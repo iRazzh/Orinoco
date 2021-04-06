e2c4753517a7553153cc10d7178e69a0973c7cd7
iRazzh <70963008+iRazzh@users.noreply.github.com>
2021-04-06 15:23:30 +0200

    confirm sass + responsive ok

--- js/confirm.js
+++ js/confirm.js
@@ -1,13 +1,16 @@
 /* Page confirm */
 
+//===============================================
+
 // Si l"order" de sessionStorage est différent de "null"
 if (sessionStorage.getItem("order") != null) {
   // On vient le parse (analyse d'une chaîne de caractère en JSON)
   let confirm = JSON.parse(sessionStorage.getItem("order"));
   // On récupère l'ID et on remplace le texte avec l'orderId
-  document.getElementById("orderId").innerHTML = confirm.orderId;
-  document.getElementById("mail").innerHTML = confirm.contact.email;
-  document.getElementById("address").innerHTML =  "Livraison : " + confirm.contact.address + " " + confirm.contact.city;
+  document.getElementsByClassName("orderId")[0].innerHTML = confirm.orderId;
+  document.getElementsByClassName("mail")[0].innerHTML = confirm.contact.email;
+  document.getElementsByClassName("address")[0].innerHTML = confirm.contact.address + " " + confirm.contact.city;
+  document.getElementsByClassName("identity")[0].innerHTML = confirm.contact.lastName + " " + confirm.contact.firstName;
   // On crée une variable "somme" à 0 pour le calcul du prix total
   let somme = 0;
   // Pour chaque "article" dans "products" dans le sessionStorage, on l'additionne
@@ -15,10 +18,13 @@ if (sessionStorage.getItem("order") != null) {
     somme += article.price / 100;
     console.log(somme)
     // On affiche le total de la somme
-    document.getElementById("price").textContent = "Prix total : " + somme + "€";
+    document.getElementsByClassName("price")[0].textContent = "Prix total : " + somme + "€";
   })
+  sessionStorage.clear();
 } else {
   // S'il y a un souci, on envoie une alerte et on redirige le client sur la page d'accueil
   alert("Un problème est survenu, vous allez être redirigé automatiquement vers la page d'accueil.");
   window.location = 'index.html';
 }
+
+//===============================================
